756310f5c521860980d7219bdf0679d916d53f97
flamacrea <49087606+flamacrea@users.noreply.github.com>
2019-04-01 00:06:11 -0500

    correccion

---
+++ precache-manifest.b2ed9dad2cc86e969242e19de855a6e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8079599239f7b995c29994aa2d10eb4e",
+    "url": "/flamacrea/static/media/logo.80795992.png"
+  },
+  {
+    "revision": "9b20c3d65ea8d6c5c865",
+    "url": "/flamacrea/static/js/runtime~main.f0416c66.js"
+  },
+  {
+    "revision": "48e1b3aedede8b6a272d",
+    "url": "/flamacrea/static/js/main.a9af0d6d.chunk.js"
+  },
+  {
+    "revision": "2baf03935ddd869be8a8",
+    "url": "/flamacrea/static/js/2.38a21a43.chunk.js"
+  },
+  {
+    "revision": "48e1b3aedede8b6a272d",
+    "url": "/flamacrea/static/css/main.06fa7e28.chunk.css"
+  },
+  {
+    "revision": "2baf03935ddd869be8a8",
+    "url": "/flamacrea/static/css/2.a22201b4.chunk.css"
+  },
+  {
+    "revision": "aefccbf5fee2a6a04825f866bf445fc6",
+    "url": "/flamacrea/index.html"
+  }
+];
