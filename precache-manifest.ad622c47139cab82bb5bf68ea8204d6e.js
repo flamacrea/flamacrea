d2ed0ba0e497a8a2a387cc3d928a0d31197b095a
flamacrea <49087606+flamacrea@users.noreply.github.com>
2019-04-02 13:40:16 -0500

    primera version

---
+++ precache-manifest.ad622c47139cab82bb5bf68ea8204d6e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "918c2d700910af9519bdb29552bf0551",
+    "url": "/flamacrea/static/media/logo315.918c2d70.png"
+  },
+  {
+    "revision": "8079599239f7b995c29994aa2d10eb4e",
+    "url": "/flamacrea/static/media/logo.80795992.png"
+  },
+  {
+    "revision": "0899368ee7bce0b8640ea40e3149574d",
+    "url": "/flamacrea/static/media/Champagne & Limousines.0899368e.ttf"
+  },
+  {
+    "revision": "9b20c3d65ea8d6c5c865",
+    "url": "/flamacrea/static/js/runtime~main.f0416c66.js"
+  },
+  {
+    "revision": "3b9ee812396db06cf550",
+    "url": "/flamacrea/static/js/main.542a0224.chunk.js"
+  },
+  {
+    "revision": "4fdbe5cc1646ec5f3932",
+    "url": "/flamacrea/static/js/2.a2dd8104.chunk.js"
+  },
+  {
+    "revision": "3b9ee812396db06cf550",
+    "url": "/flamacrea/static/css/main.d7ea93e8.chunk.css"
+  },
+  {
+    "revision": "4fdbe5cc1646ec5f3932",
+    "url": "/flamacrea/static/css/2.a22201b4.chunk.css"
+  },
+  {
+    "revision": "b00ab228f8b963abe9ec8432c24f63be",
+    "url": "/flamacrea/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09078bfb237b2eba9615a522e625cb25.js"
+  "/flamacrea/precache-manifest.ad622c47139cab82bb5bf68ea8204d6e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/flamacrea/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
